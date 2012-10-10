57b2b60dcfe17559c346b6039c1da8cfb1ff1ea1
jinkle <cwjenkins@gmail.com>
2012-10-10 02:04:00 -0700

    rough early version with audio only

--- src/org/jinkle/jac/radio/MainActivity.java
+++ src/org/jinkle/jac/radio/MainActivity.java
@@ -1,8 +1,29 @@
+/*
+ * JAC Radio online radio streamer (jacradio.com.au)
+ * Copyright (C) 2012 Christopher Jenkins
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.jinkle.jac.radio;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +31,10 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Uri uri = Uri.parse("http://sbs-stream.sbs.uq.edu.au:8000/jacradio.mp3");
+        MediaPlayer mPlayer = MediaPlayer.create(this, uri);
+        mPlayer.start();
     }
 
     @Override
